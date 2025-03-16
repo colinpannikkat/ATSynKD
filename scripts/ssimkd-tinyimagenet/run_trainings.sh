57760be315041e3cf80f9563732efcefb992b2a7
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-03-15 17:58:31 -0700

    Add new SSIM Loss

---
+++ scripts/ssimkd-tinyimagenet/run_trainings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dir="scripts/ssimkd-tinyimagenet/"
+
+# Array of training scripts
+scripts=("train1.sh")
+scripts=("train2.sh" "train3.sh" "train4.sh")
+
+# Run each script sequentially in the background
+for script in "${scripts[@]}"; do
+    echo "Running ${script}"
+    echo "Start time: $(date)" >> "${dir}log/${script}.log"
+    nohup bash "${dir}$script" >> "${dir}log/${script}.log" 2>&1 &
+    pid=$!
+    wait $pid  # Wait for the current training to finish before proceeding
+done
+
+echo "All trainings completed."
